5f8881713943825cf64f2e3a124e2c01b75ee4b2
Miraflor <warren.miraflor@hartmannph.com>
2024-07-16 15:36:59 +0800

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          React Dev
+          Animated Website with React & Framer Motion
         </motion.span>
         <div className="social">
           <a href="#">
